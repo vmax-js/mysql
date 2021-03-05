88dfa8d18c1bcad68772f402efa7bf19313652a0
vmax-js <771835525@qq.com>
2021-03-06 00:15:30 +0800

    ok

--- routes/init.js
+++ routes/init.js
@@ -3,19 +3,33 @@
 const express = require('express');
 
 const app = express();
-app.use('/abc',require('./staticMiddleware'));
-app.get('/api',(req,res,next)=>{
-    console.log('handle1');
-    // res.send('sss');
-    // throw new Error('错误');
-    next(new Error('错误'))
-    // next();
+const path = require('path');
+
+const staticRoot = path.resolve(__dirname,'../public');
+
+app.use(express.static(staticRoot,{
+    index:'index.html', //默认是这个
+}))
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json());
+app.get('/api/student',(req,res)=>{
+    console.log(req.body);
 })
-app.get('/api',(req,res,next)=>{
-    console.log('handle3');
-    res.send('123');
-    next();
-})
+// app.use('/abc',require('./staticMiddleware'));
+// app.get('/api',(req,res,next)=>{
+//     console.log('handle1');
+//     // res.send('sss');
+//     // throw new Error('错误');
+//     next(new Error('错误'))
+//     // next();
+// })
+// app.get('/api',(req,res,next)=>{
+//     console.log('handle3');
+//     res.send('123');
+//     next();
+// })
+
 app.use('/api',require('./errorMiddleware'));
 app.listen(12345,()=>{
     console.log('server listen on 12345');
